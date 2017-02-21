29c8c5b534d9199d11a59b5168cef29ef3cae07a
ramiro <lidnele@hotmail.com>
2017-02-21 18:37:43 -0300

    actividades-addModal

--- imports/ui/admin/actividades/addActivityModal.js
+++ imports/ui/admin/actividades/addActivityModal.js
@@ -1,1 +1,8 @@
 import './addActivityModal.html';
+import { Template } from 'meteor/templating'
+
+Template.addActivityModal.rendered=function() {
+  $('#my-datepicker').datepicker({
+    format: "dd/mm/yyyy"
+  });
+}
