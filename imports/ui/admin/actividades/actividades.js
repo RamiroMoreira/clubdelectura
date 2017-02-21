f540d85c5cfc5de8a49c25c890d27d41e03ae900
ramiro <lidnele@hotmail.com>
2017-02-21 18:19:09 -0300

    actividades-addModal

--- imports/ui/admin/actividades/actividades.js
+++ imports/ui/admin/actividades/actividades.js
@@ -1,2 +1,10 @@
 import { Template } from 'meteor/templating'
+import './addActivityModal.js'
 import './actividades.html';
+
+
+Template.actividades.events({
+  'click .addActivity': function(){
+    Modal.show("addActivityModal");
+  }
+})
