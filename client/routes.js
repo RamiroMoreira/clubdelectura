e64cdfa04a9196818c932a2a77c2109f170acd27
ramiro <lidnele@hotmail.com>
2017-02-21 12:03:48 -0300

    navbar menu

--- client/routes.js
+++ client/routes.js
@@ -1,6 +1,7 @@
 import '/imports/ui/admin/adminHome.html';
 import '/imports/ui/users/home.html';
 import '/imports/ui/admin/login/login.js';
+import '/imports/ui/admin/navbar.js'
 
 var OnBeforeActions = {
   loginRequired: function() {
@@ -20,16 +21,53 @@ Router.onBeforeAction(OnBeforeActions.loginRequired, {
   only: ['admin']
 });
 
+// Router.configure({
+//   // the default layout
+//   layoutTemplate: 'mainNav'
+// });
+
 Router.route('/', function () {
   this.render('Home');
 });
 //
 Router.route('/admin', function () {
   this.render('AdminHome');
+  this.layout('mainNav');
+
 });
 
 Router.route('/login', function () {
   this.render('Login');
+});
+
+Router.route('/admin/acontecimientos', function () {
+  // this.render('AdminHome');
+  this.layout('mainNav');
+
+});
+
+Router.route('/admin/review', function () {
+  // this.render('AdminHome');
+  this.layout('mainNav');
+
+});
+
+Router.route('/admin/quienesSomos', function () {
+  // this.render('AdminHome');
+  this.layout('mainNav');
+
+});
+
+Router.route('/admin/aliados', function () {
+  // this.render('AdminHome');
+  this.layout('mainNav');
+
+});
+
+Router.route('/admin/contacto', function () {
+  // this.render('AdminHome');
+  this.layout('mainNav');
+
 });
 
 // Router.setTemplateNameConverter(_.identity);
