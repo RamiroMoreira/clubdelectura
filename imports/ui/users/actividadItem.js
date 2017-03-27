42feac10c94e4437b97d3e0afbeba5377a983bce
ramiro <lidnele@hotmail.com>
2017-03-27 19:26:18 -0300

    added text for activities

--- imports/ui/users/actividadItem.js
+++ imports/ui/users/actividadItem.js
@@ -5,14 +5,14 @@ import { Template } from 'meteor/templating';
 Template.actividadItem.onRendered(function(){
   var activity = this.data;
   var div = this.find('div.event-timeLine');
-  var base = 3000;
-  var offset = 1000;
+  var base = 2900;
+  var offset = 900;
 
   div.setAttribute("data-0","display:none;top:200%")
   div.setAttribute("data-"+(base+activity.position*offset),"display:block")
   div.setAttribute("data-"+(base+500+activity.position*offset),"display:block;top:0%")
   div.setAttribute("data-"+(base+1000+activity.position*offset),"top:0%")
   div.setAttribute("data-"+(base+1500+activity.position*offset),"display:none;top:-100%")
-  SK.refresh(div);
+  SK.refresh([div]);
 });
 
