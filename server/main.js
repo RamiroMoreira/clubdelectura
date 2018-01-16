8611c5f65ab1e9139310b05e463c6a865b8be6fc
RamiroMoreira <lidnele@hotmail.com>
2018-01-16 01:03:09 -0300

    added galeria de fotos

--- server/main.js
+++ server/main.js
@@ -6,7 +6,11 @@ import '/imports/api/colaboradores/colaboradores.js'
 import '/imports/api/markers/markers.js'
 
 Meteor.startup(() => {
+    Meteor.users.remove({});
  if(!Meteor.users.findOne()){
-   Accounts.createUser({username:"AdminName", email:"admin@hotmail.com", password:"admin123"});
+   var userId = Accounts.createUser({username:Meteor.settings.AdminName, email:Meteor.settings.AdminEmail, password:Meteor.settings.AdminPass});
+   console.log(userId);
+   // Meteor.users.update({_id:userId},{$set:{'emails.$.verified':true}});
+
  }
 });
