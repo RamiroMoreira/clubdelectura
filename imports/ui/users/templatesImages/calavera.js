f54a374810bae4ab9b476db5a5f8559acb93b88f
RamiroMoreira <lidnele@hotmail.com>
2018-04-16 21:21:47 -0300

    added new designs

---
+++ imports/ui/users/templatesImages/calavera.js
@@ -0,0 +1,16 @@
+import '/imports/ui/users/templatesImages/calavera.html'
+import './textActivity.js'
+
+Template.calavera.helpers({
+    esPar: function(){
+        if(this.position % 2 === 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    },
+    'isDesktop':function(){
+        return !(( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false ) || (navigator.userAgent.toLowerCase().indexOf("android") > -1)) ;
+    }
+})
---
+++ imports/ui/users/templatesImages/pantalla.js
@@ -0,0 +1,16 @@
+import '/imports/ui/users/templatesImages/pantalla.html'
+import './textActivity.js'
+
+Template.pantalla.helpers({
+    esPar: function(){
+        if(this.position % 2 === 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    },
+    'isDesktop':function(){
+        return !(( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false ) || (navigator.userAgent.toLowerCase().indexOf("android") > -1)) ;
+    }
+})
