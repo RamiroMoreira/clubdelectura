e0fd56bdf36b734ecee378d23e384affb8cff980
RamiroMoreira <lidnele@hotmail.com>
2018-04-28 04:16:24 -0300

    working on movile

--- imports/ui/users/templatesImages/musculoso.js
+++ imports/ui/users/templatesImages/musculoso.js
@@ -1,6 +1,38 @@
 import '/imports/ui/users/templatesImages/musculoso.html'
 import './textActivity.js'
 
+Template.musculoso.onCreated(() => {
+    $(document).on('keyup', (e) => {
+        console.log(e.key)
+        if(window.localStorage.getItem('estadoMusculoso') == "ArrowDown"){
+           if(e.key == "ArrowLeft"){
+               window.localStorage.setItem('estadoMusculoso', "ArrowLeft")
+           }
+        }
+        else if(window.localStorage.getItem('estadoMusculoso') == "ArrowLeft"){
+            if(e.key == "ArrowRight")
+            window.localStorage.setItem('estadoMusculoso', "ArrowRight")
+        }
+        else if(window.localStorage.getItem('estadoMusculoso') == "ArrowRight"){
+            if(e.key == "x"){
+                alert("You fucking win")
+                window.localStorage.setItem('estadoMusculoso', "");
+            }
+        }
+        else{
+            if(e.key == "ArrowDown"){
+                window.localStorage.setItem('estadoMusculoso', "ArrowDown");
+                Meteor.setTimeout(function(){
+                    window.localStorage.setItem('estadoMusculoso', "");
+                },2000)
+            }
+        }
+    });
+});
+Template.musculoso.onDestroyed(() => {
+    $(document).off('keyup');
+});
+
 Template.musculoso.helpers({
     esPar: function(){
         if(this.position % 2 === 0){
@@ -13,4 +45,8 @@ Template.musculoso.helpers({
     'isDesktop':function(){
         return !(( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false ) || (navigator.userAgent.toLowerCase().indexOf("android") > -1)) ;
     }
+})
+
+Template.musculoso.events({
+
 })
